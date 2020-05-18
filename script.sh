1e744987bbf85ef5da2bbb526f4b5a90161c3145
Edvanio Filho <j.edvaniofilho@gmail.com>
2020-05-17 21:33:06 -0300

    Tinha feito merda

--- script.sh
+++ script.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-# ----------------------------- VARIÁVEIS ----------------------------- #
-PPA_LIBRATBAG="ppa:libratbag-piper/piper-libratbag-git"
-PPA_GRAPHICS_DRIVERS="ppa:graphics-drivers/ppa"
-
 ## Removendo travas eventuais do apt ##
 
 sudo rm /var/lib/dpkg/lock-frontend ; sudo rm /var/cache/apt/archives/lock ;
+
+## Adicionar repositório ##
+
+sudo sh -c 'wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -' ;
+sudo sh -c 'wget -qO- https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list' ;
 
 ## Atualizando o repositório ##
 
 sudo apt update &&
 
-## suporte a snaps para o mint. Para Ubuntu comentar a linha
+## suporte a snaps para o mint. Para Ubuntu comentar a linha ##
 
 sudo apt install snapd &&
 
 ## Instalando pacotes e programas do repositório deb do Ubuntu (Mint) ##
 
-sudo apt install python3 python-pip conda dart &&
+sudo apt install python3 python-pip dart &&
 
 ## Instalando pacotes do Snap ##
 
@@ -26,7 +27,6 @@ sudo snap install skype --classic &&
 sudo snap install code --classic &&  
 sudo snap install pycharm-professional --classic &&
 sudo snap install spotify --classic &&
-sudo snap install ubuntu-make &&
 sudo snap install android-studio --classic &&
 sudo snap install inkscape &&
 sudo snap install gimp --classic &&
